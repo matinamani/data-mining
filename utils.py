2c6f13cd1193000c1d0d0db40315fd80460fc87b
Matin Amani <mtin.amani@gmail.com>
2024-02-06 21:47:14 +0330

    feat: add speedy_knn function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 
 def k_means(data, k):
@@ -48,3 +49,33 @@ def knn(test_data, train_data, k):
     )
 
     return np.argpartition(distances, k, axis=1)[:, :k]
+
+
+def speedy_knn(df, centroids, k):
+    data = np.concatenate(
+        (df.values, np.zeros((df.values.shape[0], 1))), axis=1
+    )
+
+    # adding predicted labels to data
+    data[:, -1] = k_means_predict(data[:, :4], centroids)[1]
+
+    labels = []
+
+    for sample in data:
+        cluster = data[data[:, -1] == sample[-1]]
+        distance = np.sqrt(
+            np.sum(
+                np.square(cluster[:, :4] - sample[:4]),
+                axis=1,
+            )
+        )
+
+        nearest_samples = (
+            cluster
+            if len(cluster) <= k + 1
+            else cluster[np.argpartition(distance, k + 1)[: k + 1]]
+        )
+
+        labels.append(stats.mode(nearest_samples[:, -2]).mode)
+
+    return np.array(labels, dtype="int16")
